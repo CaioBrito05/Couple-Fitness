72f429106e402b430acca7eba97fffd4cb6867f0
CaioAgrojet <caio@agrojet.com.br>
2026-04-10 11:03:30 -0300

    - Estilos mudados

--- ContadorAcademia/src/App.jsx
+++ ContadorAcademia/src/App.jsx
@@ -20,62 +20,65 @@ function App() {
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Couple Fitness + 1</h1>
-          <p>
-            Contador de vezes que fomos na academia
-          </p>
-          <h2>
-            Edição Mês de {meses[mesAtual]}
-          </h2>
-        </div>
-
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="contadores">
-          <div id="">
-            <h2>Caio</h2>
-            <button
-              className="counter"
-              onClick={() => setCountC((countC) => countC + 1)}
-            >
-              Count is {countC}
-            </button>
+      <div className='bg-dark text-white min-vh-100'>
+        <>
+          <div className="hero">
+            <img src={heroImg} className="base" width="170" height="179" alt="" />
+            <img src={reactLogo} className="framework" alt="React logo" />
+            <img src={viteLogo} className="vite" alt="Vite logo" />
           </div>
           <div>
-            <h2>Maitê</h2>
-            <button
-              className="counter"
-              onClick={() => setCountM((countM) => countM + 1)}
-            >
-              Count is {countM}
-            </button>
-          </div>
-          <div>
-            <h2>Yaluê</h2>
-            <button
-              className="counter"
-              onClick={() => setCountY((countY) => countY + 1)}
-            >
-              Count is {countY}
-            </button>
+            <h1 className="mb-4">Couple Fitness + 1</h1>
+            <p>
+              Contador de vezes que fomos na academia
+            </p>
+            <h2>
+              Edição Mês de {meses[mesAtual]}
+            </h2>
           </div>
 
+        </>
 
-        </div>
-      </section>
+        <div className="ticks"></div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+        <section id="next-steps">
+          <div className="row">
+            <div className="col-md-4">
+              <h2>Caio</h2>
+              <button
+                className="counter"
+                onClick={() => setCountC((countC) => countC + 1)}
+              >
+                Count is {countC}
+              </button>
+            </div>
+            <div className="col-md-4">
+              <h2>Maitê</h2>
+              <button
+                className="counter"
+                onClick={() => setCountM((countM) => countM + 1)}
+              >
+                Count is {countM}
+              </button>
+            </div>
+            <div className="col-md-4">
+              <h2>Yaluê</h2>
+              <button
+                className="counter"
+                onClick={() => setCountY((countY) => countY + 1)}
+              >
+                Count is {countY}
+              </button>
+            </div>
+
+
+          </div>
+        </section>
+
+        <div className="ticks"></div>
+        <section id="spacer"></section>
+      </div>
+
     </>
   )
 }
